f1f9af33e5a82dff9bc2092e1fed9273b03534ea
mverzi <maggie.verzi@gmail.com>
2023-12-03 13:57:51 -0500

    Alt tag updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ import LinkedInIcon from "../images/linkedin-icon.png"
 export default function Footer(){
     return (
         <footer>
-            <a href="https://twitter.com/mverzi1" target="_blank" rel="noreferrer" alt="Twitter Icon"><img src={TwitterIcon} /></a>
-            <a href="https://linkedin.com/in/mary-verzi" target="_blank" rel="noreferrer" alt="LinkedIn Icon"><img src={LinkedInIcon} /></a>
-            <a href="https://github.com/mverzi" target="_blank" rel="noreferrer" alt="Github Icon"><img src={GithubIcon} /></a>
+            <a href="https://twitter.com/mverzi1" target="_blank" rel="noreferrer"><img src={TwitterIcon} alt="Twitter Icon"/></a>
+            <a href="https://linkedin.com/in/mary-verzi" target="_blank" rel="noreferrer"><img src={LinkedInIcon} alt="LinkedIn Icon"/></a>
+            <a href="https://github.com/mverzi" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon"/></a>
         </footer>
     )
 }
